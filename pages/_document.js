0bd107fea446ded43c41be00c8b2bc1c544ce1d2
newworldhead <newworldhead@gmail.com>
2021-07-06 17:44:33 +0100

    import css with vercel seems to not work?

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=IM+Fell+DW+Pica&family=Poppins:wght@500&display=swap" rel="stylesheet" />
-
+                    <link rel="stylesheet" href="https://unpkg.com/react-quill@1.3.3/dist/quill.snow.css"></link>
                 </Head>
                 <body>
                     <Main />
