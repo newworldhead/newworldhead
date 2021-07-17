384ae51b655f032a8057f48e69f2ae87b95cc663
newworldhead <newworldhead@gmail.com>
2021-07-17 11:02:13 +0100

    Added Gtag

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../utils/gtag'
 import '../styles/globals.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { AuthProvider } from '@/context/AuthContext'
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import { GA_TRACKING_ID } from '../utils/gtag'
+
 class MyDocument extends Document {
     render() {
         return (
             <Html>
                 <Head>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GA_TRACKING_ID}', {
+                            page_path: window.location.pathname,
+                            });
+                        `,
+                        }}
+                    />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=IM+Fell+DW+Pica&family=Poppins:wght@500&display=swap" rel="stylesheet" />
                     <link rel="stylesheet" href="https://unpkg.com/react-quill@1.3.3/dist/quill.snow.css"></link>
